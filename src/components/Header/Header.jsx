00189320caf48cc563395b2a7475ee10c1d6223f
gmmonsalve <gbrielamonsalve@gmail.com>
2024-09-17 23:46:16 -0500

    Delete unused href

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
 
 function Header(){
     return (
         <Navbar expand="lg" data-bs-theme="dark"  bg="dark">
         <Container>
-            <Navbar.Brand href="#home">Security Front</Navbar.Brand>
+            <Navbar.Brand>Security Front</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
