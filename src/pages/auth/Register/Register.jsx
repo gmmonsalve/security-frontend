9a2679f6c50880b2045224c9411622b4ed8f2104
gmmonsalve <gbrielamonsalve@gmail.com>
2024-09-18 21:49:38 -0500

    Update register page

--- src/pages/auth/Register/Register.jsx
+++ src/pages/auth/Register/Register.jsx
@@ -1,11 +1,16 @@
 import RegisterForm from "../../../components/Auth/RegisterForm";
+import Card from 'react-bootstrap/Card';
 
 
 function Register(){
     return <>
         <section>
             <article>
-                <RegisterForm></RegisterForm>
+            <Card className='container-sm'>
+                    <Card.Body>
+                        <RegisterForm></RegisterForm>
+                    </Card.Body>
+                </Card>
             </article>
         </section>
     </>
