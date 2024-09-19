4af86f5c210f816a58cc425a2f81abdb75725790
gmmonsalve <gbrielamonsalve@gmail.com>
2024-09-18 21:50:35 -0500

    Add api connection handle register

--- src/components/auth/RegisterForm/RegisterForm.jsx
+++ src/components/auth/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,25 @@
+import {environment,routes} from "../../../utils/constants";
+import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 function RegisterForm(){
+
+    const navigate = useNavigate()
 
     const handleOnSubmit = async event=>{
         
         event.preventDefault()
         const form = event.target
+        const resetForm = (form)=>{
+            form.username.value = ""
+            form.password.value = ""
+            form.email.value = ""
+            form.password.value = ""
+            form.confirmpass.value = ""
+            form.name.value = ""
+       }
 
         const registerInfo = {
             name: form.name.value,
@@ -12,23 +28,64 @@ function RegisterForm(){
             password: form.password.value
         }
 
-        /* console.log(registerInfo) */
+        try {
+            const register = await fetch(`${environment.apiUrl}${routes.register}`, {
+              method: 'POST',
+              headers: {
+                "Content-Type": 'application/json'
+              },
+              body: JSON.stringify(registerInfo)
+            })
+      
+            if (!register.ok){ 
+              resetForm(form)
+              return toast.error('Error en la peticion al servidor')
+            }
+      
+            const response = await register.json()
+      
+            if (!response.process) {
+                resetForm(form)
+                return toast.error('Error al registrar al usuario')
+            }
+      
+            toast.success('Usuario registrado con exito')
+      
+            localStorage.setItem('user', response.data)//TODO: Review a safer option
+      
+            navigate('/')
+      
+          } catch (error) {
+            toast.error('Error en la peticion al servidor')
+            resetForm(form)
+          }
+
     } //TODO: Add styles and handle API connection.
 
     return <>
-        <form onSubmit={handleOnSubmit}>
-            <label htmlFor="name">Nombre</label>
-            <input type="text" name="name" placeholder="Nombre" required/>
-            <label htmlFor="username"></label>
-            <input type="text" name="username" placeholder="Nombre de usuario" required/>
-            <label htmlFor="email"></label>
-            <input type="email" name="email" placeholder="correo" required/>
-            <label htmlFor="password" ></label>
-            <input type="password" name="password" placeholder="password" required/>
-            <label htmlFor="confirmPassword"></label>
-            <input type="password" name="confirmPassword" placeholder="confirmar password" required/>
-            <button type="submit">Registrar</button>
-        </form>
+    <Form onSubmit={handleOnSubmit}>
+        <Form.Group controlId="name">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" placeholder="Nombre"  required></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="username">
+            <Form.Label>Nombre de Usuario</Form.Label>
+            <Form.Control type="text" placeholder="Username"  required></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Email" required></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="password" placeholder="Contraseña" required></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="confirmpass">
+            <Form.Label>Confirmar contraseña</Form.Label>
+            <Form.Control type="password" placeholder="Confirmar contraseña" required></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="primary">Registrar</Button>
+    </Form>
     </>
 }
 
