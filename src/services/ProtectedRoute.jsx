756b06c0ea995c58b79f4c1ed339903e2b485e86
Laura Marcela Arango Correa <laura.arango@mercadolibre.com.co>
2024-09-19 21:48:46 -0500

    comment changes

--- src/services/ProtectedRoute.jsx
+++ src/services/ProtectedRoute.jsx
@@ -2,8 +2,35 @@ import isAuth from "./isAuth"
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ component })=>{
-    return isAuth()? component: <Navigate to="/login"/>;
+const ProtectedRoute = ({ component }) => {
+    const authenticated = isAuth();
+
+    return authenticated ? component : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
+
+/*
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import isAuth from './isAuth';
+
+const ProtectedRoute = ({ component }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+    useEffect(() => {
+        const authStatus = isAuth();
+        setIsAuthenticated(authStatus);
+    }, []);
+
+    if (isAuthenticated === null) {
+        // Mostrar un mensaje de carga mientras se verifica la autenticación
+        return <div>Loading...</div>;
+    }
+
+    // Si está autenticado, renderiza el componente, si no, redirige al registro o login
+    return isAuthenticated ? component : <Navigate to="/register" />;
+};
+
+export default ProtectedRoute;
+*/
