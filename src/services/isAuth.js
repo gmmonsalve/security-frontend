f4aefe6a8d762e1c91e30c4d9f71b4f4d99a2809
lmarangoc <laura.arango@mercadolibre.com.co>
2024-09-19 21:13:06 -0500

    Added token lifetime validations (#3)

--- src/services/isAuth.js
+++ src/services/isAuth.js
@@ -1,7 +1,23 @@
 import storeSession from "./storeSession";
+import jwt_decode from "jwt-decode";
 
-function isAuth(){
-   return !!storeSession.getCookie('auth')
+function isAuth() {
+   const token = storeSession.getCookie('auth');
+
+   if (!token) return false;
+
+   try {
+      const decoded = jwt_decode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+
+      if (decoded.exp && decoded.exp > currentTime) {
+         return true;
+      } else {
+         return false;
+      }
+   } catch (error) {
+      return false;
+   }
 }
 
 export default isAuth;
