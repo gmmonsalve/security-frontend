873746269ed73e960e48850aa1812cc850b29713
Laura Marcela Arango Correa <laura.arango@mercadolibre.com.co>
2024-09-19 20:29:05 -0500

    Added regex validations

--- src/components/auth/RegisterForm/RegisterForm.jsx
+++ src/components/auth/RegisterForm/RegisterForm.jsx
@@ -1,26 +1,27 @@
-import {environment,routes} from "../../../utils/constants";
+import { environment, routes } from "../../../utils/constants";
 import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import storeSession from "../../../services/storeSession";
+import { user_regex } from "../../../services/validations/auth.validation";
 
-function RegisterForm(){
+function RegisterForm() {
 
     const navigate = useNavigate()
 
-    const handleOnSubmit = async event=>{
-        
+    const handleOnSubmit = async event => {
+
         event.preventDefault()
         const form = event.target
-        const resetForm = (form)=>{
+        const resetForm = (form) => {
             form.username.value = ""
             form.password.value = ""
             form.email.value = ""
             form.password.value = ""
             form.confirmpass.value = ""
-            form.name.value = ""
-       }
+            
+        }
 
         const registerInfo = {
             name: form.name.value,
@@ -31,62 +32,62 @@ function RegisterForm(){
 
         try {
             const register = await fetch(`${environment.apiUrl}${routes.register}`, {
-              method: 'POST',
-              headers: {
-                "Content-Type": 'application/json'
-              },
-              body: JSON.stringify(registerInfo)
+                method: 'POST',
+                headers: {
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify(registerInfo)
             })
-      
-            if (!register.ok){ 
-              resetForm(form)
-              return toast.error('Error en la peticion al servidor')
+
+            if (!register.ok) {
+                resetForm(form)
+                return toast.error('Error en la peticion al servidor')
             }
-      
+
             const response = await register.json()
-      
+
             if (!response.process) {
                 resetForm(form)
                 return toast.error('Error al registrar al usuario')
             }
-      
+
             toast.success('Usuario registrado con exito')
-      
-            storeSession.setCookie('auth',`${response}`, {secure: true, expires: null,sameSite: 'Strict', path: '/'})
-      
+
+            storeSession.setCookie('auth', `${response}`, { secure: true, expires: null, sameSite: 'Strict', path: '/' })
+
             navigate('/')
-      
-          } catch (error) {
+
+        } catch (error) {
             toast.error('Error en la peticion al servidor')
             resetForm(form)
-          }
+        }
 
     } //TODO: Add styles and handle API connection.
 
     return <>
-    <Form onSubmit={handleOnSubmit}>
-        <Form.Group controlId="name">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="Nombre"  required></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="username">
-            <Form.Label>Nombre de Usuario</Form.Label>
-            <Form.Control type="text" placeholder="Username"  required></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Email" required></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="password">
-            <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" placeholder="Contraseña" required></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="confirmpass">
-            <Form.Label>Confirmar contraseña</Form.Label>
-            <Form.Control type="password" placeholder="Confirmar contraseña" required></Form.Control>
-        </Form.Group>
-        <Button type="submit" variant="primary">Registrar</Button>
-    </Form>
+        <Form onSubmit={handleOnSubmit}>
+            <Form.Group controlId="name">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type="text" placeholder="Nombre" required></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="username">
+                <Form.Label>Nombre de Usuario</Form.Label>
+                <Form.Control type="text" placeholder="Username" required></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Email" required></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="password">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control type="password" placeholder="Contraseña" required></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="confirmpass">
+                <Form.Label>Confirmar contraseña</Form.Label>
+                <Form.Control type="password" placeholder="Confirmar contraseña" required></Form.Control>
+            </Form.Group>
+            <Button type="submit" variant="primary">Registrar</Button>
+        </Form>
     </>
 }
 
