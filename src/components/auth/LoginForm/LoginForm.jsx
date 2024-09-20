8bfc65e7fd0f9b2e387088a3a62a5644633ae5ce
yuliethz <yuliethz@uninorte.edu.co>
2024-09-19 21:08:33 -0500

    estilos aplicados al login

--- src/components/auth/LoginForm/LoginForm.jsx
+++ src/components/auth/LoginForm/LoginForm.jsx
@@ -1,79 +1,80 @@
-import {environment,routes} from "../../../utils/constants"
-import { toast } from 'react-toastify'
-import { useNavigate } from "react-router-dom" 
+import { environment, routes } from "../../../utils/constants";
+import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import storeSession from "../../../services/storeSession";
 import Alert from 'react-bootstrap/Alert';
+import style from './LoginForm.module.css';
 
-function LoginForm(){
+function LoginForm() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const handleOnSubmit = async event => {
+    event.preventDefault();
+    const form = event.target;
+    const resetForm = (form) => {
+      form.username.value = "";
+      form.password.value = "";
+    };
 
-    const handleOnSubmit = async event=>{
+    const loginInfo = {
+      nickname: form.username.value,
+      password: form.password.value
+    };
 
-        event.preventDefault()
-        const form = event.target 
-        const resetForm = (form)=>{
-             form.username.value = ""
-             form.password.value = ""
-        }
+    try {
+      const login = await fetch(`${environment.apiUrl}${routes.login}`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(loginInfo)
+      });
 
-        const loginInfo = {
-            nickname: form.username.value,
-            password: form.password.value
-        }
+      if (!login.ok) {
+        resetForm(form);
+        return toast.error('Error en la petición al servidor');
+      }
 
-        try {
-            const login = await fetch(`${environment.apiUrl}${routes.login}`, {
-              method: 'POST',
-              headers: {
-                "Content-Type": 'application/json'
-              },
-              body: JSON.stringify(loginInfo)
-            })
-      
-            if (!login.ok){ 
-              resetForm(form)
-              return toast.error('Error en la peticion al servidor')
-            }
-      
-            const response = await login.json()
-      
-            if (!response.process) {
-                resetForm(form)
-                return toast.error('Error al loguear usuario')
-            }
-      
-            toast.success('Usuario logueado con exito')
-      
-            storeSession.setCookie('auth',`${response}`, {secure: true, expires: null,sameSite: 'Strict', path: '/'})
-      
-            navigate('/')
-      
-          } catch (error) {
-            toast.error('Error en la peticion al servidor')
-            resetForm(form)
-          }
-      
+      const response = await login.json();
+
+      if (!response.process) {
+        resetForm(form);
+        return toast.error('Error al loguear usuario');
+      }
+
+      toast.success('Usuario logueado con éxito');
+
+      storeSession.setCookie('auth', `${response}`, { secure: true, expires: null, sameSite: 'Strict', path: '/' });
+
+      navigate('/');
+    } catch (error) {
+      toast.error('Error en la petición al servidor');
+      resetForm(form);
     }
-      //TODO: Add styles and handle API connection.
-    return <>
-            <Form onSubmit={handleOnSubmit}>
-                <Form.Group className="mb-3" controlId="username">
-                    <Form.Label>Nombre de usuario</Form.Label>
-                    <Form.Control type="text" placeholder="username" required/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="password" required/>
-                </Form.Group>
-                <Button type="submit" variant="primary">Login</Button>{' '}
-            </Form>
-            <Alert key={'dark'} variant={'dark'}>
-                  ¿No tienes una cuenta? <Alert.Link href="/register">Regístrate</Alert.Link>
-            </Alert>
-    </>
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.formWrapper}>
+        <Form onSubmit={handleOnSubmit}>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label>Nombre de usuario</Form.Label>
+            <Form.Control type="text" placeholder="username" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="password" placeholder="password" required />
+          </Form.Group>
+          <Button type="submit" className={style.loginButton}>Login</Button>{' '}
+        </Form>
+        <Alert key={'dark'} variant={'dark'} className={style.alertWrapper}>
+          ¿No tienes una cuenta? <Alert.Link href="/register">Regístrate</Alert.Link>
+        </Alert>
+      </div>
+    </div>
+  );
 }
 
 export default LoginForm;
